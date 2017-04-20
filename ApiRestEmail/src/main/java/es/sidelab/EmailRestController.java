0520e6a552744926125601b40c5ab80274a0f12c
Óscar repiso <scar repiso>
2017-04-20 17:42:00 +0200

    service fixed

--- ApiRestEmail/src/main/java/es/sidelab/EmailRestController.java
+++ ApiRestEmail/src/main/java/es/sidelab/EmailRestController.java
@@ -6,6 +6,7 @@ import javax.mail.MessagingException;
 import javax.mail.PasswordAuthentication;
 import javax.mail.Session;
 import javax.mail.Transport;
+import javax.mail.internet.AddressException;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 
@@ -22,49 +23,63 @@ import org.springframework.web.bind.annotation.RestController;
 public class EmailRestController {
 
 	
-	
-	
 	@RequestMapping(value = "/registro_nuevo", method = RequestMethod.POST)
 	public ResponseEntity<String> sendEmail(@RequestParam("email") String email,@RequestParam("subject") String subject, @RequestParam("body") String body){
-		 System.out.println(email);
-		 System.out.println(subject);
-		 System.out.println(body);
-		 	final String username = "vitualcoach@gmail.com";
-	        final String password = "urjc1995";
-
-	        Properties props = new Properties();
-	        props.put("mail.smtp.starttls.enable", "true");
-	        props.put("mail.smtp.auth", "true");
-	        props.put("mail.smtp.host", "smtp.gmail.com");
-	        props.put("mail.smtp.port", "587");
-	        props.put("mail.smtp.ssl.trust", "smtp.gmail.com");
-
-	        Session session = Session.getInstance(props,
-	          new javax.mail.Authenticator() {
-	            protected PasswordAuthentication getPasswordAuthentication() {
-	                return new PasswordAuthentication(username, password);
-	            }
-	          });
-
-	        try {
-
-	            Message message = new MimeMessage(session);
-	            message.setFrom(new InternetAddress("vitualcoach@gmail.com"));
-	            message.setRecipients(Message.RecipientType.TO,
-	                InternetAddress.parse(email));
-	            message.setSubject(subject);
-	            message.setText(body);
-
-	            Transport.send(message);
-
-	            System.out.println("Done");
-
-	        } catch (MessagingException e) {
-	            throw new RuntimeException(e);
-	        }
 		
+		if(!isValidEmailAddress(email)){
+			
+			return new ResponseEntity<>("La dirección de email no es válida",HttpStatus.BAD_REQUEST);
+			
+		}
 		
-		return new ResponseEntity<>("Hola que ase",HttpStatus.NOT_FOUND);
+	 	final String username = "vitualcoach@gmail.com";
+        final String password = "urjc1995";
+
+        Properties props = new Properties();
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.port", "587");
+        props.put("mail.smtp.ssl.trust", "smtp.gmail.com");
+
+        Session session = Session.getInstance(props,
+          new javax.mail.Authenticator() {
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(username, password);
+            }
+          });
+
+        try {
+
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress("vitualcoach@gmail.com"));
+            message.setRecipients(Message.RecipientType.TO,
+                InternetAddress.parse(email));
+            message.setSubject(subject);
+            message.setText(body);
+
+            Transport.send(message);
+
+            System.out.println("Done");
+
+        } catch (MessagingException e) {
+            e.printStackTrace();
+            return new ResponseEntity<>("Ha habido un error al enviar",HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+	
+	
+        return new ResponseEntity<>("Mensaje enviado correctamente",HttpStatus.ACCEPTED);
+	}
+	
+	public static boolean isValidEmailAddress(String email) {
+	       boolean result = true;
+	       try {
+	          InternetAddress emailAddr = new InternetAddress(email);
+	          emailAddr.validate();
+	       } catch (AddressException ex) {
+	          result = false;
+	       }
+	       return result;
 	}
 
 
